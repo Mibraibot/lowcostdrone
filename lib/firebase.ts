95b6436c0384cf45dfb48ff0e86cc426957463ad
ibot <happyhomekost@gmail.com>
2026-02-06 09:40:51 +0700

    fix3

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -1,3 +1,5 @@
+"use client";
+
 import { initializeApp, getApps, getApp } from "firebase/app";
 import { getDatabase } from "firebase/database";
 import { getAnalytics, isSupported } from "firebase/analytics";
